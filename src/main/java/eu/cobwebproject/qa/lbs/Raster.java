ccdf0475cdd1e8988c3a4d3015df82ea5dfaea39
sebastianclarke <sebastian.clarke@envsys.co.uk>
2015-12-22 23:23:15 +0000

    Raster loading sanitised and extracted into Raster class, seems to be more stable Tests running loading both NRW and original surface model and getting sane results

---
+++ src/main/java/eu/cobwebproject/qa/lbs/Raster.java
@@ -0,0 +1,203 @@
+package eu.cobwebproject.qa.lbs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Raster {
+	private Parameters params;
+	private double[][] surfaceModel;
+	private String fileName = null;
+
+	/**
+	 * Construct a raster with the parameters and data already parsed.
+	 * note: The String fileName constructor is preferred. 
+	 * 
+	 * @param p A parsed parsed Parameter object
+	 * @param surfaceModel The surface model as a 2d double array
+	 */
+	public Raster(Parameters p, double[][] surfaceModel) {
+		this.params = p;
+		this.surfaceModel = surfaceModel;
+	}
+	
+	/**
+	 * Return a Raster parsed from the file path specified as string
+	 * 
+	 * @param file A String of the path to the file to parse
+	 * @throws IOException If the file cant be found or anything else goes wrong whilst reading
+	 */
+	public Raster(String file) throws IOException {
+		this.fileName = file;						// set the file name
+		
+		double[] headerData = readRasterHeader();	// read from the file
+		this.params = new Parameters((int)headerData[4], (int)headerData[0], 
+                (int)headerData[1], headerData[2], headerData[3], headerData[5]);
+		this.surfaceModel = readAsciiData();		// read from the file
+	}
+	
+	public Parameters getParams() {
+		return params;
+	}
+	
+	public double[][] getSurfaceModel() {
+		return surfaceModel;
+	}
+	
+	private double[] readRasterHeader() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		
+		String DSMline = null;
+		
+		double var;
+		double[] headerData = new double[6];
+		
+		try {
+			for(int i = 0; i < 6; i++){
+							
+				DSMline = br.readLine();
+				
+				char[] buffer = new char[DSMline.length()];
+				String s = null;
+				
+				switch (i){
+				case 0: 
+					
+					DSMline.getChars(6,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;
+					
+				case 1: 
+			
+					DSMline.getChars(6,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;
+				
+				case 2: 
+					
+					DSMline.getChars(9,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;
+					
+				case 3: 
+					
+					DSMline.getChars(9,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;
+					
+				case 4: 
+					
+					DSMline.getChars(8,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;
+					
+				case 5: 
+					
+					DSMline.getChars(12,DSMline.length(), buffer, 0);
+					s = String.valueOf(buffer);
+					var = Double.parseDouble(s);
+					headerData[i] = var;
+					break;						
+				}
+			}
+		} finally {
+			br.close();
+		}
+		
+		return headerData;
+	}
+	
+	private double[][] readAsciiData() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		double[] headerData = new double[6];
+		String DSMline;
+		double var;
+
+		for(int i = 0; i < 6; i++){
+						
+			DSMline = br.readLine();
+			
+			char[] buffer = new char[DSMline.length()];
+			String s = null;
+			
+			switch(i) {
+			case 0: 
+				
+				DSMline.getChars(6,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;
+				
+			case 1: 
+		
+				DSMline.getChars(6,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;
+			
+			case 2: 
+				
+				DSMline.getChars(9,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;
+				
+			case 3: 
+				
+				DSMline.getChars(9,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;
+				
+			case 4: 
+				
+				DSMline.getChars(8,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;
+				
+			case 5: 
+				
+				DSMline.getChars(12,DSMline.length(), buffer, 0);
+				s = String.valueOf(buffer);
+				var = Double.parseDouble(s);
+				headerData[i] = var;
+				break;					
+		
+			}		
+		} 
+				
+		double[][] ASCIIData = new double[(int) headerData[0]][(int) headerData[1]];
+		try {
+			for(int i = 0;i < headerData[0];i++){
+				DSMline = br.readLine();
+				String [] temp = new String[(int) headerData[1]];
+				temp = DSMline.split("[ ]+");
+				for(int j = 0;j < headerData[1];j++){
+					ASCIIData[i][j] = Double.parseDouble(temp[j]);				
+			 	}
+			}
+		} finally {
+			br.close();
+		}
+		
+		return ASCIIData;
+	}
+}
--- src/test/java/eu/cobwebproject/qa/lbs/LineOfSightTest.java
+++ src/test/java/eu/cobwebproject/qa/lbs/LineOfSightTest.java
@@ -1,5 +1,6 @@
 package eu.cobwebproject.qa.lbs;
 
+import java.io.IOException;
 import java.net.URL;
 
 import org.junit.Test;
@@ -56,51 +57,75 @@ public class LineOfSightTest extends TestCase{
     }
     
     @Test
-    public void testCompareDataset() {
-    	URL url = this.getClass().getResource("surfaceModelNRW.asc");
-    	//Raster rast = RasterReader.ReadRaster(url.getFile());
+    public void testRasterClassLoading() {
+    	URL asciiFile1 = this.getClass().getResource("surfaceModel.txt");
+    	String testFile1 = asciiFile1.getFile().toString(); 	// this is the original surface model Sam provided
     	
-    	RasterReader rr = new RasterReader(url.getFile().toString());
-        //Raster rast = RasterReader.ReadRaster(url.getFile().toString());
-        
-        double headerData[] = new double[6];
-        
-        headerData = rr.getASCIIHeader();
-        
-        double surfaceModel[][] = new double[(int) headerData[0]][(int) headerData[1]];
-        
-        surfaceModel = rr.getASCIIData();
+    	URL asciiFile2 = this.getClass().getResource("surfaceModelNRW.asc");
+    	String testFile2 = asciiFile2.getFile().toString(); 	// this is the NRW 1M dataset of the same area
+    	
+    	Raster raster1 = null;
+    	Raster raster2 = null;
+    	
+    	try {
+			raster1 = new Raster(testFile1);
+			System.out.println("File 1: "+ testFile1);
+			raster2 = new Raster(testFile2);
+			System.out.println("File 2: "+ testFile2);
+		} catch (IOException e) {
+			fail("IOException: " + e.getMessage());
+		}
 
-        Parameters parameters = new Parameters((int)headerData[4], (int)headerData[0], 
-                (int)headerData[1], headerData[2], headerData[3], headerData[5]);
+    	double easting, northing, bearing, tilt, myHeight;
+    	double[] result;
+    	LineOfSightCoordinates loS;
+   
+    	// set up and run test conditions for raster 1
+    	/*
+    	easting = 265365;
+        northing = 289115;
+        bearing = 0;
+        tilt = -1;
+        myHeight = 1.5;*/
+    	easting = 265114.674984; 
+        northing = 289276.72543;
+        bearing = 0;
+        tilt = -1;
+        myHeight = 1.5;
 
-    	
-        double easting = 265114.674984; 
-        double northing = 289276.72543;
-    	
-        double bearing = 0;
-        double tilt = -1;
-        double myHeight = 1.5;
-        double myCoords[] = new double[2];
-        
-        myCoords[0] = easting;
-        myCoords[1] = northing; 
-        
-        LineOfSightCoordinates loS = new LineOfSightCoordinates(
-                myCoords,
-                surfaceModel,
-                parameters,
+        loS = new LineOfSightCoordinates(
+                new double[]{easting,northing},
+                raster1.getSurfaceModel(),
+                raster1.getParams(),
                 bearing,
                 tilt,
                 myHeight);
         
-        double []result;// = new double[5];
+        result = loS.getMyLoSResult();
+        System.out.println("result " + result[0] + " " + result[1] + " " + result[2] + " " + result[3] + " " + result[4]);
+        /*
+        assertEquals(8.1, result[0]);
+        assertEquals(49.43426, result[1]);
+        assertEquals(265365.0, result[2]);
+        assertEquals(289123.0, result[3]);
+        assertEquals(45.0, result[4]);*/
+                
+        // set up and run test 2
+        easting = 265114.674984; 
+        northing = 289276.72543;
+        bearing = 0;
+        tilt = -1;
+        myHeight = 1.5;
+        
+        loS = new LineOfSightCoordinates(
+                new double[]{easting,northing},
+                raster2.getSurfaceModel(),
+                raster2.getParams(),
+                bearing,
+                tilt,
+                myHeight);
         
         result = loS.getMyLoSResult();
         System.out.println("result " + result[0] + " " + result[1] + " " + result[2] + " " + result[3] + " " + result[4]);
-        
-     
     }
-    
- 
 }
